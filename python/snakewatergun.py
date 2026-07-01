3b51855bf1c3ccb81b80daad712fe30cabbd9be5
Manya <manyamahajn147@gmail.com>
2026-07-01 22:44:47 +0530

    Update snakewatergun.py

---
+++ python/snakewatergun.py
@@ -0,0 +1,26 @@
+import random
+
+compnum = random.choice([-1, 0, 1])
+usernum = int(input("Enter the no : "))
+numdic = {-1 : "snake" , 0 : "water", 1 : "gun"}
+num=numdic[usernum]
+print(f"U choose {num} and computer choose {numdic[compnum]}")
+
+if(compnum==num):
+    print("DRAW")
+
+else : 
+    if(compnum==1 and usernum==-1) :
+        print("U LOSE")
+    elif(compnum==1 and usernum==0) :
+        print("U WIN")
+    if(compnum==0 and usernum==-1) :
+        print("U WIN")
+    elif(compnum==0 and usernum==1) :
+        print("U LOSE")
+    if(compnum==-1 and usernum==1) :
+        print("U WIN")
+    elif(compnum==-1 and usernum==0) :
+        print("U LOSE")
+    else :
+        print("ERROR")
